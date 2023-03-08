c1dcf8535beaad910395d4bcffe5c07096f0e4a5
mayanksomani7 <83702471+mayanksomani7@users.noreply.github.com>
2023-03-08 06:49:42 +0530

    update dbConnection.js

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const dbConnection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: 3307,
-    database: 'rems'
+    host: 'bdz4rxonx3qtlhxsuifj-mysql.services.clever-cloud.com',
+    user: 'u8y6mdskfirbsurn',
+    password: '52qArNrtUq6h15AmPBI5',
+    port: 3306,
+    database: 'bdz4rxonx3qtlhxsuifj'
 });
 
 module.exports = dbConnection.promise();
